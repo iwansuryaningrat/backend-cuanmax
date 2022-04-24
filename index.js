52830a52afb6b379967467ff8bb930bca54318ea
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-04-24 23:39:02 +0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Express REST server
+const express = require("express");
+const cors = require("cors");
+import "dotenv/config";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
