c0db6ef0b8091b45233f94396533fff18b2d8b68
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-04-30 02:21:44 +0700

    Add Mongoose

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Express REST server
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 // Load .env file
 require("dotenv").config();
 
@@ -8,11 +9,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+// MongoDB Connection
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on port ${process.env.PORT}`);
+  console.log(`Server started on port http//localhost:${process.env.PORT}`);
 });
