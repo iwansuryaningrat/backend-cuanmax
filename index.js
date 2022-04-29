6c8446a0fae25781a00aa2afe788f802366b6aec
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-04-30 01:52:05 +0700

    Update main file

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 // Express REST server
 const express = require("express");
 const cors = require("cors");
-import "dotenv/config";
+// Load .env file
+require("dotenv").config();
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method");
+  res.send("Hello World!");
 });
 
-app.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
 });
-
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
-
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
-});
-
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
