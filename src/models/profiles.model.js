4424f4d72c4a43d5bc81f4c3aa1fa18264b4e5f4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-10 08:32:27 +0700

    Create Profiles Model

---
+++ src/models/profiles.model.js
@@ -0,0 +1,37 @@
+module.exports = (mongoose) => {
+  const schema = mongoose.Schema(
+    {
+      name: String,
+      username: String,
+      email: String,
+      password: String,
+      image: String,
+      type: {
+        accountType: {
+          type: String,
+          startDate: Date,
+          endDate: Date,
+        },
+        isAdmin: Boolean,
+      },
+      referal: {
+        referalCode: String,
+        referalLink: String,
+        referalCount: Number,
+        referalAccount: [{ username: String }],
+      },
+    },
+    {
+      timestamp: true,
+    }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Profiles = mongoose.model("profiles", schema);
+  return Profiles;
+};
