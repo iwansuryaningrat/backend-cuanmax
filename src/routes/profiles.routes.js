dad35955a2668f2ae1eb648c8b8bb1065664a05f
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-14 12:47:18 +0700

    Create Profile Controllers & Routers

--- src/routes/profiles.routes.js
+++ src/routes/profiles.routes.js
@@ -3,6 +3,9 @@ module.exports = (app) => {
   const router = require("express").Router();
 
   router.get("/", profiles.findAll);
+  router.get("/:id", profiles.findOne);
+  router.post("/:id", profiles.update);
+  router.delete("/:id", profiles.delete);
 
   app.use("/api/v1/profiles", router);
 };
