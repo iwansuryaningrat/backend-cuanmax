39bd7694ddfc5f7ea2b98b90d11fb0f452cf6dd8
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-15 16:09:34 +0700

    Update signup route

--- src/routes/signup.routes.js
+++ src/routes/signup.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
   const signup = require("../controllers/signup.controller");
   const router = require("express").Router();
 
-  router.get("/", signup.findAll);
+  router.post("/", signup.create);
 
   app.use("/api/v1/signup", router);
 };
