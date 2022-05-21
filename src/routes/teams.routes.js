cbe87caddbe44e79ea07cfd603a8e27b40e60575
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-21 14:41:56 +0700

    Create teams routes:

---
+++ src/routes/teams.routes.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+  const teams = require("../controllers/teams.controller");
+  const router = require("express").Router();
+
+  router.get("/", teams.findAll);
+  router.get("/:id", teams.findOne);
+  router.delete("/:id", teams.delete);
+
+  app.use("/api/v1/teams", router);
+};
