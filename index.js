75cffb6d7b90ff1dfcf67516699d566b2bdec3fb
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-24 23:22:15 +0700

    Update Users Routes

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const videoConfig = require("./src/configs/videoUploader.config");
 const app = express();
 
 app.use(cors());
-app.use(bodyParser());
 app.use(express.json());
 app.use(
   express.urlencoded({
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,7 +4,6 @@ module.exports = (app) => {
 
   router.get("/", users.findAll);
   router.get("/:id", users.findOne);
-  router.post("/:id", users.update);
   router.delete("/:id", users.delete);
 
   app.use("/api/v1/users", router);
