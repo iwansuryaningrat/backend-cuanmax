29241c4854238dd22cd8da6d21b13566db88b316
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-24 23:58:29 +0700

    Create Testimoni Controller

---
+++ src/controllers/testimoni.controller.js
@@ -0,0 +1,50 @@
+const db = require("../models/index");
+const Testimoni = db.testimoni;
+
+exports.findAll = (req, res) => {
+  Testimoni.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error while retrieving testimoni.",
+      });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Testimoni.findById(id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error while showing testimoni.",
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Testimoni.findByIdAndRemove(id)
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Testimoni not found",
+        });
+      }
+
+      res.send({
+        message: "Testimoni was deleted",
+      });
+    })
+    .catch((err) => {
+      res.status(409).send({
+        message: err.message || "Some error while delete testimoni.",
+      });
+    });
+};
