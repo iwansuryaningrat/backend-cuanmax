7d1db066d905fc6d7c30adff8a07a3965535eddb
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-25 00:24:00 +0700

    Make Testimoni Create Controller

--- src/controllers/testimoni.controller.js
+++ src/controllers/testimoni.controller.js
@@ -27,6 +27,27 @@ exports.findOne = (req, res) => {
     });
 };
 
+exports.create = (req, res) => {
+  const testimoni = new Testimoni({
+    name: req.body.name,
+    position: req.body.position,
+    company: req.body.company,
+    body: req.body.body,
+    photosUrl: req.file.path,
+  });
+
+  testimoni
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error while creating testimoni.",
+      });
+    });
+};
+
 exports.delete = (req, res) => {
   const id = req.params.id;
 
