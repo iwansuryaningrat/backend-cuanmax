640bdbdf82d8553050fa7c07ce25439588355512
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-26 01:58:42 +0700

    Make update route for services

--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -5,6 +5,7 @@ module.exports = (app) => {
   router.get("/", services.findAll);
   router.post("/", services.create);
   router.get("/:id", services.findOne);
+  router.put("/:id", services.update);
   router.delete("/:id", services.delete);
 
   app.use("/api/v1/services", router);
