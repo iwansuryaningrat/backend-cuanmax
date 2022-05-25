ecbda16271433ed663ea9be4d0766677c30e1bc4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-26 01:49:59 +0700

    Make Create Controller for Services

--- src/controllers/services.controller.js
+++ src/controllers/services.controller.js
@@ -12,3 +12,29 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+exports.create = (req, res) => {
+  const services = new Services({
+    serviceName: req.body.serviceName,
+    description: req.body.description,
+    image: req.file.path,
+    benefit: [
+      {
+        benefitName: req.body.benefitName,
+      },
+    ],
+  });
+
+  services
+    .save()
+    .then((result) => {
+      res.status(200).send({
+        message: "Service successfully added.",
+      });
+    })
+    .catch((err) => {
+      res.status(409).send({
+        message: err.message || "Some error while creating service.",
+      });
+    });
+};
