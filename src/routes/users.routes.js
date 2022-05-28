709027bfdad9c9205bd14ed726c986afb5b74f98
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-29 03:32:37 +0700

    Update User Route
    Try to add auth into get all users route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,9 @@
 module.exports = (app) => {
   const users = require("../controllers/users.controller");
+  const auth = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", users.findAll);
+  router.get("/", auth.auth, users.findAll);
   router.get("/:id", users.findOne);
   router.delete("/:id", users.delete);
 
