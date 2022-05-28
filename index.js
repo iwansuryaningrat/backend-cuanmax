be18a96f03d52c2b68578e5cfdfc931f3d2d121e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-29 02:33:22 +0700

    Add auth routes into main file

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ app.listen(process.env.PORT, () => {
 require("./src/routes/playlists.routes")(app); // Playlist Router
 require("./src/routes/users.routes")(app); // Users Router
 require("./src/routes/videos.routes")(app); // Videos Router
-require("./src/routes/signup.routes")(app); // Signup Router
 require("./src/routes/subscribers.routes")(app); // Subscribers Router
 require("./src/routes/message.routes")(app); // Message Router
 require("./src/routes/pricing.routes")(app); // Pricing Router
@@ -61,3 +60,4 @@ require("./src/routes/teams.routes")(app); // Teams Router
 require("./src/routes/vouchers.routes")(app); // Vouchers Router
 require("./src/routes/testimoni.routes")(app); // Testimoni Router
 require("./src/routes/services.routes")(app); // Services Router
+require("./src/routes/auth.routes")(app); // Auth Router
