178806184fc9b47cdecaeac88e00467126ec84ac
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-29 02:17:46 +0700

    Create Login Controller

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -3,3 +3,53 @@ const Users = db.users;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const bcrypt = require("bcrypt");
+
+exports.login = (req, res) => {
+  // Validate request
+  if (!req.body.email && !req.body.password) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+  }
+
+  Users.findOne({
+    email: req.body.email,
+  })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({
+          message: "User not found",
+        });
+      } else {
+        bcrypt.compare(req.body.password, user.password, (err, result) => {
+          if (result) {
+            const token = jwt.sign(
+              {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                admin: user.type.isAdmin,
+              },
+              process.env.JWT_SECRET,
+              {
+                expiresIn: "1h",
+              }
+            );
+            res.status(200).send({
+              message: "User logged in",
+              token: token,
+            });
+          } else {
+            res.status(401).send({
+              message: "Password incorrect",
+            });
+          }
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the User.",
+      });
+    });
+};
