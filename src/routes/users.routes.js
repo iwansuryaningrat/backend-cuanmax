e82912481aa42818df57faa9a98e9c6658ceef0d
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-05-31 01:28:34 +0700

    Update Users Routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,8 +3,9 @@ module.exports = (app) => {
   const auth = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", auth.auth, users.findAll);
-  router.get("/:id", auth.auth, users.findOne);
+  router.get("/", users.findAll);
+  router.get("/:id", users.findOne);
+  router.post("/:id", auth.auth, users.update);
   router.delete("/:id", auth.auth, users.delete);
 
   app.use("/api/v1/users", router);
