0b8d6b9fef1cd8b0a08f27c0f328aa20f01d1146
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-02 20:35:48 +0700

    Restructuring Working Folders
    Membuat struktur folder baru pada controller untuk menghimpun controller dari coinmarketcap dan midtrans

--- src/routes/coinmarketcap.routes.js
+++ src/routes/coinmarketcap.routes.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  const coinmarketcap = require("../controllers/coinmarketcap.controller");
+  const coinmarketcap = require("../controllers/coinmarketcap/coinmarketcap.controller");
   const router = require("express").Router();
 
   router.get("/latest", coinmarketcap.latest);
