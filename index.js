347779635767b408888febf0039550e7c813cc71
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-06 21:10:03 +0700

    Create User Routes
    Create User Routes & Add it into main file

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.listen(process.env.PORT, () => {
 
 // Routers
 // require("./src/routes/playlists.routes")(app); // Playlist Router
-// require("./src/routes/users.routes")(app); // Users Router
+require("./src/routes/users.routes")(app); // Users Router
 // require("./src/routes/videos.routes")(app); // Videos Router
 // require("./src/routes/subscribers.routes")(app); // Subscribers Router
 // require("./src/routes/message.routes")(app); // Message Router
---
+++ src/routes/users.routes.js
@@ -0,0 +1,12 @@
+module.exports = (app) => {
+  const users = require("../controllers/users.controller");
+  const auth = require("../middlewares/auth");
+  const router = require("express").Router();
+
+  router.get("/", auth.auth, users.findAll);
+  router.get("/:id", auth.auth, users.findOne);
+  router.post("/:id", auth.auth, users.update);
+  router.delete("/:id", auth.auth, users.delete);
+
+  app.use("/api/v1/users", router);
+};
