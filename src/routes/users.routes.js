2b5c0bd71c8d1f51e89f28f4364ac9ffc1127517
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-06 23:47:33 +0700

    Fixing Users Routes
    Edit Change Password Route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
   router.get("/", auth.auth, users.findAll);
   router.get("/:id", auth.auth, users.findOne);
   router.post("/:id", auth.auth, users.update);
-  router.post("/changePassword/:id", auth.auth, users.changePassword);
+  router.post("/:id/changepassword", auth.auth, users.changePassword);
   router.delete("/:id", auth.auth, users.delete);
 
   app.use("/api/v1/users", router);
