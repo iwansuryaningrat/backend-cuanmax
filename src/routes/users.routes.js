23271598795e6dff259e2e58ad9ae1a3136b6915
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-07 02:17:42 +0700

    Update Users Route
    Change some route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -6,7 +6,8 @@ module.exports = (app) => {
   router.get("/", auth.auth, users.findAll);
   router.get("/:id", auth.auth, users.findOne);
   router.post("/:id", auth.auth, users.update);
-  router.post("/:id/changepassword", auth.auth, users.changePassword);
+  router.put("/:id/changepassword", auth.auth, users.changePassword);
+  router.put("/:id/changepicture", users.changeProfilePicture);
   router.delete("/:id", auth.auth, users.delete);
 
   app.use("/api/v1/users", router);
