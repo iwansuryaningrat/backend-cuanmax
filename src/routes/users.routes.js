a31aae326c84f0467e387641e732224321e5782a
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-07 03:58:41 +0700

    Update Users Route
    Add Referal checking route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,10 +4,11 @@ module.exports = (app) => {
   const router = require("express").Router();
 
   router.get("/", auth.auth, users.findAll);
+  router.get("/:username/:referal", auth.auth, users.referal);
   router.get("/:id", auth.auth, users.findOne);
   router.post("/:id", auth.auth, users.update);
   router.put("/:id/changepassword", auth.auth, users.changePassword);
-  router.put("/:id/changepicture", users.changeProfilePicture);
+  router.put("/:id/changepicture", auth.auth, users.changeProfilePicture);
   router.delete("/:id", auth.auth, users.delete);
 
   app.use("/api/v1/users", router);
