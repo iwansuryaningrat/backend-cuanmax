dc7b3586d261eeb8c0b8ce768c101d6e393f43c3
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-07 13:13:51 +0700

    Create Message Model
    Create Message Model & Add Model into db config

---
+++ src/models/message.model.js
@@ -0,0 +1,37 @@
+module.exports = (mongoose) => {
+  const schema = mongoose.Schema(
+    {
+      firstName: {
+        type: String,
+        require: true,
+      },
+      lastName: String,
+      email: {
+        type: String,
+        require: true,
+      },
+      subject: {
+        type: String,
+        require: true,
+      },
+      message: {
+        type: String,
+        require: true,
+      },
+      status: {
+        type: String,
+        require: true,
+      },
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Message = mongoose.model("message", schema);
+  return Message;
+};
