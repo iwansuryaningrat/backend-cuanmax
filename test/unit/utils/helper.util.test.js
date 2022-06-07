94dc104f35b5015b8d8c394cc492fa7e70beac2c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-07 15:50:39 +0700

    Update TEST

--- test/unit/utils/helper.util.test.js
+++ test/unit/utils/helper.util.test.js
@@ -1,7 +1,10 @@
 // require("dotenv").config();
-const day = new Date().getDate();
-const month = new Date().getMonth() + 1;
-const year = new Date().getFullYear();
-const date = `${day}/${month}/${year}`;
+// const day = new Date().getDate();
+// const month = new Date().getMonth() + 1;
+// const year = new Date().getFullYear();
+// const date = `${day}/${month}/${year}`;
+
+// console.log(date);
+const date = new Date("2020-01-01 10:00:00").toString();
 
 console.log(date);
