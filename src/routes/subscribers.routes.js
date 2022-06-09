54dcc4ef00ee0e1c7b363a1f58d008af31a2d5e4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-10 01:58:40 +0700

    Update Subscribers Routes
    Fixing All Endpoint & Add Auth

--- src/routes/subscribers.routes.js
+++ src/routes/subscribers.routes.js
@@ -1,11 +1,13 @@
 module.exports = (app) => {
   const subscribers = require("../controllers/subscribers.controller");
+  const auth = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", subscribers.findAll);
-  router.get("/:id", subscribers.findOne);
-  router.post("/", subscribers.create);
-  router.delete("/:id", subscribers.delete);
+  router.get("/", auth.auth, subscribers.findAll);
+  router.get("/:id", auth.auth, subscribers.findOne);
+  router.post("/", auth.auth, subscribers.create);
+  router.put("/:id", auth.auth, subscribers.update);
+  router.delete("/:id", auth.auth, subscribers.delete);
 
   app.use("/api/v1/subscribers", router);
 };
