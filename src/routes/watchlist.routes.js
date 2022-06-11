0e5b16ddc4a93df63f3e50b1990f888d91896d4f
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-12 00:23:53 +0700

    Update Watchlist Routes

--- src/routes/watchlist.routes.js
+++ src/routes/watchlist.routes.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
   router.get("/", auth.auth, watchlist.findAll);
   router.get("/:id", auth.auth, watchlist.findOne);
   router.delete("/:id", auth.auth, watchlist.delete);
-  router.get("/id/nonactivate", auth.auth, watchlist.nonActivate);
+  router.get("/:id/nonactivate", auth.auth, watchlist.nonActivate);
 
   app.use("/api/v1/watchlist", router);
 };
