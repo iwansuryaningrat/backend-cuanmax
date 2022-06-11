d064c8eede2ceaa54b85750c0b786ee8ccb4b1e0
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-12 00:49:40 +0700

    Update Watchlist Routes
    Add Create Data Endpoint

--- src/routes/watchlist.routes.js
+++ src/routes/watchlist.routes.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
   const router = require("express").Router();
 
   router.get("/", auth.auth, watchlist.findAll);
+  router.post("/", auth.auth, watchlist.create);
   router.get("/:id", auth.auth, watchlist.findOne);
   router.delete("/:id", auth.auth, watchlist.delete);
   router.put("/:id/nonactivate", auth.auth, watchlist.nonActivate);
