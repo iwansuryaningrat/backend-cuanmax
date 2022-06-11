c1d37427097144ad2d7e3896b874034c9429eb9e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-11 21:20:05 +0700

    Update Watchlist Controller
    Create nonactivated function

--- src/controllers/watchlist.controller.js
+++ src/controllers/watchlist.controller.js
@@ -78,3 +78,32 @@ exports.delete = (req, res) => {
       });
     });
 };
+
+exports.nonActivate = (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return res.status(400).send({
+      message: "Watchlist ID is required",
+    });
+  }
+
+  Watchlist.findByIdAndUpdate(id, { isActive: false })
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Watchlist not found",
+        });
+      }
+
+      res.send({
+        message: "Watchlist was non-activated",
+        timestamp: new Date().toString(),
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error while delete watchlist.",
+      });
+    });
+};
