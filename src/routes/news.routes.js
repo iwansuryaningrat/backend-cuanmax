52d726c53767f1880dacc5892b68af754de74474
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-13 02:08:57 +0700

    Create News Routes

---
+++ src/routes/news.routes.js
@@ -0,0 +1,7 @@
+module.exports = (app) => {
+  const news = require("../controllers/news.controller");
+  const auth = require("../middlewares/auth");
+  const router = require("express").Router();
+
+  app.use("/api/v1/news", router);
+};
