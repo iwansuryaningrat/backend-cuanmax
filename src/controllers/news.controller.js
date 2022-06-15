50aba48c2f01ea9817fbc65e199716e354a1e8df
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-16 04:07:11 +0700

    Update News Controller
    Create Find All News

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,2 +1,18 @@
 const db = require("../models/index");
 const News = db.news;
+
+exports.findAll = (req, res) => {
+  News.find()
+    .then((news) => {
+      res.send({
+        message: "All news were fetched successfully",
+        timestamp: new Date().toISOString(),
+        data: news,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving news.",
+      });
+    });
+};
