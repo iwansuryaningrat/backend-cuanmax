868b29a91bebbe27abc9b8eee3d793ec56f102f1
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-16 02:32:41 +0700

    Update Vouchers Routes
    Create Find All Route

--- src/routes/vouchers.routes.js
+++ src/routes/vouchers.routes.js
@@ -1,8 +1,9 @@
 module.exports = (app) => {
   const vouchers = require("../controllers/vouchers.controller");
+  const { auth } = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", vouchers.findAll);
+  router.get("/", auth, vouchers.findAll);
 
   app.use("/api/v1/vouchers", router);
 };
