1f781bcaa538de48cc9bcb2cd26c5854d3f2f61b
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-16 02:52:04 +0700

    Update Vouchers Routes
    Create Delete Data Endpoint

--- src/routes/vouchers.routes.js
+++ src/routes/vouchers.routes.js
@@ -1,10 +1,15 @@
 module.exports = (app) => {
-  const { findAll, create } = require("../controllers/vouchers.controller");
+  const {
+    findAll,
+    create,
+    deleteVoucher,
+  } = require("../controllers/vouchers.controller");
   const { auth } = require("../middlewares/auth");
   const router = require("express").Router();
 
   router.get("/", auth, findAll);
   router.post("/", auth, create);
+  router.delete("/:id", auth, deleteVoucher);
 
   app.use("/api/v1/vouchers", router);
 };
