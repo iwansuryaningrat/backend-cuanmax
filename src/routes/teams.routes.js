aab2344e803e27dc441abf881cd9ba430af27e6b
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-16 02:35:39 +0700

    Update Teams Routes
    Fixing Auth Issue

--- src/routes/teams.routes.js
+++ src/routes/teams.routes.js
@@ -1,14 +1,14 @@
 module.exports = (app) => {
   const teams = require("../controllers/teams.controller");
-  const auth = require("../middlewares/auth");
+  const { auth } = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", auth.auth, teams.findAll);
-  router.post("/", auth.auth, teams.create);
-  router.get("/:id", auth.auth, teams.findOne);
-  router.put("/:id", auth.auth, teams.update);
-  router.put("/:id/teamprofile", auth.auth, teams.teamProfile);
-  router.delete("/:id", auth.auth, teams.delete);
+  router.get("/", auth, teams.findAll);
+  router.post("/", auth, teams.create);
+  router.get("/:id", auth, teams.findOne);
+  router.put("/:id", auth, teams.update);
+  router.put("/:id/teamprofile", auth, teams.teamProfile);
+  router.delete("/:id", auth, teams.delete);
 
   app.use("/api/v1/teams", router);
 };
