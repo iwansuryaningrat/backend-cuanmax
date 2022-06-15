74340deb178240b3c3a9e96013381f0aee054119
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-16 02:58:26 +0700

    Update Vouchers Routes
    Create Find One Endpoint

--- src/routes/vouchers.routes.js
+++ src/routes/vouchers.routes.js
@@ -4,12 +4,14 @@ module.exports = (app) => {
     create,
     deleteVoucher,
     update,
+    findOne,
   } = require("../controllers/vouchers.controller");
   const { auth } = require("../middlewares/auth");
   const router = require("express").Router();
 
   router.get("/", auth, findAll);
   router.post("/", auth, create);
+  router.get("/:id", auth, findOne);
   router.delete("/:id", auth, deleteVoucher);
   router.put("/:id", auth, update);
 
