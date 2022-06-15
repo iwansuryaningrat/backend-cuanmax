6a2ad8ad84bdf8c8498a1e261a61f3e7ba5dad83
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-16 02:36:23 +0700

    Update Subscribers Routes
    Fixing Auth Issue

--- src/routes/subscribers.routes.js
+++ src/routes/subscribers.routes.js
@@ -1,13 +1,13 @@
 module.exports = (app) => {
   const subscribers = require("../controllers/subscribers.controller");
-  const auth = require("../middlewares/auth");
+  const { auth } = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", auth.auth, subscribers.findAll);
-  router.get("/:id", auth.auth, subscribers.findOne);
-  router.post("/", auth.auth, subscribers.create);
-  router.put("/:id", auth.auth, subscribers.update);
-  router.delete("/:id", auth.auth, subscribers.delete);
+  router.get("/", auth, subscribers.findAll);
+  router.get("/:id", auth, subscribers.findOne);
+  router.post("/", auth, subscribers.create);
+  router.put("/:id", auth, subscribers.update);
+  router.delete("/:id", auth, subscribers.delete);
 
   app.use("/api/v1/subscribers", router);
 };
