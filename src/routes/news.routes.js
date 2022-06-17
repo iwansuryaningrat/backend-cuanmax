f003c89d95c09f7e6fc9aaac0c4b57e850d95a2f
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-06-17 11:38:00 +0700

    Update News Routes
    Add Admin & Pro Member Auth Minddlewares

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   const { findAll } = require("../controllers/news.controller");
-  const { auth } = require("../middlewares/auth");
+  const { auth, admin, proMember } = require("../middlewares/auth");
   const router = require("express").Router();
 
   router.get("/", auth, findAll);
