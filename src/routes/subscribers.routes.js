2bfd5860d2fc93e43dc3f9963e0a9c48236d3486
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-01 03:55:03 +0700

    Update Subscribers Routes

--- src/routes/subscribers.routes.js
+++ src/routes/subscribers.routes.js
@@ -1,13 +1,19 @@
 module.exports = (app) => {
-  const subscribers = require("../controllers/subscribers.controller");
+  const {
+    findAll,
+    findOne,
+    create,
+    update,
+    deleteSubs,
+  } = require("../controllers/subscribers.controller");
   const { auth, admin, proMember } = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", auth, subscribers.findAll);
-  router.get("/:id", admin, subscribers.findOne);
-  router.post("/", subscribers.create);
-  router.put("/:id", admin, subscribers.update);
-  router.delete("/:id", admin, subscribers.delete);
+  router.get("/", auth, findAll);
+  router.get("/:id", admin, findOne);
+  router.post("/", create);
+  router.put("/:id", admin, update);
+  router.delete("/:id", admin, deleteSubs);
 
   app.use("/api/v1/subscribers", router);
 };
