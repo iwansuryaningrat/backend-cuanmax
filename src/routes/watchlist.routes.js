461b23c8065217af80944439fcad6ac30383f167
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-01 04:07:10 +0700

    Update Watchlist Routes

--- src/routes/watchlist.routes.js
+++ src/routes/watchlist.routes.js
@@ -1,14 +1,21 @@
 module.exports = (app) => {
-  const watchlist = require("../controllers/watchlist.controller");
+  const {
+    findAll,
+    findOne,
+    create,
+    deleteWl,
+    nonActivate,
+    update,
+  } = require("../controllers/watchlist.controller");
   const { admin, proMember } = require("../middlewares/auth");
   const router = require("express").Router();
 
-  router.get("/", proMember, watchlist.findAll);
-  router.post("/", admin, watchlist.create);
-  router.get("/:id", proMember, watchlist.findOne);
-  router.delete("/:id", admin, watchlist.delete);
-  router.put("/:id/nonactivate", admin, watchlist.nonActivate);
-  router.put("/:id", admin, watchlist.update);
+  router.get("/", proMember, findAll);
+  router.post("/", admin, create);
+  router.get("/:id", proMember, findOne);
+  router.delete("/:id", admin, deleteWl);
+  router.put("/:id/nonactivate", admin, nonActivate);
+  router.put("/:id", admin, update);
 
   app.use("/api/v1/watchlists", router);
 };
