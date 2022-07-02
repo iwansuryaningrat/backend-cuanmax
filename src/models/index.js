00e1f92425b91387cd98a2d21d52fff36616cb7d
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-03 00:27:04 +0700

    Create Live Class Model

--- src/models/index.js
+++ src/models/index.js
@@ -7,6 +7,7 @@ const db = {};
 
 db.mongoose = mongoose;
 db.url = dbConfig.url;
+db.liveclass = require("./liveclass.model");
 db.messages = require("./messages.model")(mongoose);
 db.news = require("./news.model")(mongoose);
 // db.playlists = require("./playlists.model")(mongoose);
