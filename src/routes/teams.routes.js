ec3dda16373de641a5a0c7799754fcfe0071b92b
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-02 19:37:46 +0700

    Update Middleware at Teams Route

--- src/routes/teams.routes.js
+++ src/routes/teams.routes.js
@@ -7,15 +7,15 @@ module.exports = (app) => {
     teamProfile,
     deleteTeam,
   } = require("../controllers/teams.controller");
-  const { admin } = require("../middlewares/auth");
+  const { login, admin } = require("../middlewares/auth");
   const router = require("express").Router();
 
   router.get("/", findAll);
   router.post("/", admin, create);
-  router.get("/:id", admin, findOne);
-  router.put("/:id", admin, update);
-  router.put("/:id/teamprofile", admin, teamProfile);
-  router.delete("/:id", admin, deleteTeam);
+  router.get("/:id", login, admin, findOne);
+  router.put("/:id", login, admin, update);
+  router.put("/:id/teamprofile", login, admin, teamProfile);
+  router.delete("/:id", login, admin, deleteTeam);
 
   app.use("/api/v1/teams", router);
 };
