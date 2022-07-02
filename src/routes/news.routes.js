d87dd39db7c37e8c09d281f143b666aa0da82618
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-02 19:59:10 +0700

    Create Update News Endpoint

--- src/routes/news.routes.js
+++ src/routes/news.routes.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
     deleteNews,
     createNews,
     findById,
+    update,
   } = require("../controllers/news.controller");
   const { login, admin } = require("../middlewares/auth");
   const router = require("express").Router();
@@ -12,6 +13,7 @@ module.exports = (app) => {
   router.post("/", login, admin, createNews);
   router.delete("/:id", login, admin, deleteNews);
   router.get("/:id", findById);
+  router.put("/:id", login, admin, update);
 
   app.use("/api/v1/news", router);
 };
