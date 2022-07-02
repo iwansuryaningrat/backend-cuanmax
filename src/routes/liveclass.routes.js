6e78667df5438630a9e0873de2c727eb7b527cca
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-03 01:10:19 +0700

    Create Delete Endpoint

--- src/routes/liveclass.routes.js
+++ src/routes/liveclass.routes.js
@@ -1,10 +1,15 @@
 module.exports = (app) => {
-  const { findAll, findOne } = require("../controllers/liveclass.controller");
+  const {
+    findAll,
+    findOne,
+    deleteClass,
+  } = require("../controllers/liveclass.controller");
   const { login, admin, proMember } = require("../middlewares/auth");
   const router = require("express").Router();
 
   router.get("/", findAll);
   router.get("/:id", findOne);
+  router.delete("/:id", login, admin, deleteClass);
 
   app.use("/api/v1/liveclass", router);
 };
