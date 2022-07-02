28306e04a73c5a6e0eb4668e35a3708c81b3d003
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-03 01:10:07 +0700

    Create Delete Function

--- src/controllers/liveclass.controller.js
+++ src/controllers/liveclass.controller.js
@@ -3,8 +3,16 @@ const Liveclass = db.liveclass;
 
 // Find all liveclasses (Done)
 exports.findAll = (req, res) => {
-  const { status } = req.query;
+  const { status, category, tags } = req.query;
   const query = status ? { status } : {};
+
+  if (category) {
+    query.category = category;
+  }
+
+  if (tags) {
+    query.tags = { $in: tags.split(",") };
+  }
 
   Liveclass.find(query)
     .sort({ createdAt: -1 })
@@ -49,6 +57,35 @@ exports.findOne = (req, res) => {
     .catch((err) => {
       return res.status(500).send({
         message: err.message || "Error while retrieving liveclass",
+      });
+    });
+};
+
+// Delete liveclass (Done)
+exports.deleteClass = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send({
+      message: "Live class Id is required",
+    });
+  }
+
+  Liveclass.findByIdAndRemove(id)
+    .then((liveclass) => {
+      if (!liveclass) {
+        return res.status(404).send({
+          message: "Liveclass not found with id " + id,
+        });
+      }
+      res.send({
+        message: "Liveclass was deleted successfully",
+        timestamp: new Date().toString(),
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message || "Error while deleting liveclass",
       });
     });
 };
