7026705917c7fc4b209339d84a4999bc14e3c3f6
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-03 00:59:58 +0700

    Create Find one by ID live class endpoint

--- src/routes/liveclass.routes.js
+++ src/routes/liveclass.routes.js
@@ -1,9 +1,10 @@
 module.exports = (app) => {
-  const { findAll } = require("../controllers/liveclass.controller");
+  const { findAll, findOne } = require("../controllers/liveclass.controller");
   const { login, admin, proMember } = require("../middlewares/auth");
   const router = require("express").Router();
 
   router.get("/", findAll);
+  router.get("/:id", findOne);
 
   app.use("/api/v1/liveclass", router);
 };
