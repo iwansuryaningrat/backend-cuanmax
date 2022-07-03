b7632e37e17a5a984cc9dbfd1f552fd9c3ddb841
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-03 11:59:46 +0700

    Create Videos Routes

---
+++ src/routes/videos.routes.js
@@ -0,0 +1,19 @@
+module.exports = (app) => {
+  const {
+    findAll,
+    findOne,
+    findByPlaylist,
+    update,
+    deleteVideo,
+  } = require("../controllers/videos.controller");
+  const { login, admin, proMember } = require("../middlewares/auth");
+  const router = require("express").Router();
+
+  router.get("/", login, proMember, findAll);
+  router.get("/:id", login, proMember, findOne);
+  router.get("/playlistId", login, proMember, findByPlaylist);
+  router.put("/:id", login, admin, update);
+  router.delete("/:id", login, admin, deleteVideo);
+
+  app.use("/api/v1/videos", router);
+};
