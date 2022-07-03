45f47ef6d5b8910a0602a8ba1b5513bbd62084a6
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-03 11:38:52 +0700

    Create Playlists Schema

---
+++ src/models/playlists.model.js
@@ -0,0 +1,44 @@
+module.exports = (mongoose) => {
+  const schema = mongoose.Schema(
+    {
+      name: {
+        type: String,
+        require: true,
+      },
+      category: {
+        type: String,
+        require: true,
+      },
+      description: String,
+      instructor: {
+        type: String,
+        require: true,
+      },
+      videoLevel: {
+        type: String,
+        require: true,
+      },
+      image: {
+        imageName: {
+          type: String,
+        },
+        imageLink: {
+          type: String,
+          require: true,
+          default: 'https://via.placeholder.com/150',
+        },
+      },
+      videoCount: Number,
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Playlists = mongoose.model("playlists", schema);
+  return Playlists;
+};
