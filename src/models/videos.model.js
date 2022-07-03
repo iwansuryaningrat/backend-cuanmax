61af72349eaf1a8258782e2ea91f0f91effa5521
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-03 11:49:23 +0700

    Create Videos Schema

---
+++ src/models/videos.model.js
@@ -0,0 +1,48 @@
+module.exports = (mongoose) => {
+  const schema = mongoose.Schema(
+    {
+      title: {
+        type: String,
+        require: true,
+      },
+      description: String,
+      url: {
+        type: String,
+        require: true,
+      },
+      thumbnail: {
+        type: String,
+        default: "",
+      },
+      playlist: {
+        playlistId: {
+          type: schema.Types.ObjectId,
+          ref: "Playlists",
+          require: true,
+        },
+        playlistTitle: {
+          type: String,
+          require: true,
+        },
+      },
+      tags: [String],
+      views: Number,
+      likes: Number,
+      dislikes: Number,
+      duration: Number,
+      category: String,
+      date: Date,
+      status: String,
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Videos = mongoose.model("videos", schema);
+  return Videos;
+};
