acc49c9c5f139a5f0cdf574f981341c5bafdf1b5
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-30 22:03:22 +0700

    Add Create Video Endpoint

--- src/routes/videos.routes.js
+++ src/routes/videos.routes.js
@@ -15,7 +15,7 @@ module.exports = (app) => {
   router.get("/:playlistId", login, proMember, findByPlaylist);
   router.put("/:id", login, admin, update);
   router.delete("/:id", login, admin, deleteVideo);
-  router.post("/", create);
+  router.post("/", login, admin, create);
 
   app.use("/api/v1/videos", router);
 };
