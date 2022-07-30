76e6820fd6a5ad25a5e7a3ab75c8aad823602469
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-07-30 20:54:12 +0700

    Image Uploader Config
    Change Filenaming

--- src/configs/imageUploader.config.js
+++ src/configs/imageUploader.config.js
@@ -8,7 +8,7 @@ exports.imageStorage = multer.diskStorage({
   filename: (req, file, cb) => {
     cb(
       null,
-      file.originalname.replace(/\s/g, "-") + "-" + new Date().getTime()
+      new Date().getTime() + "-" + file.originalname.replace(/\s/g, "-")
     );
   },
 });
