1c2252314b7292afb264f576e76617459e036d0f
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-08-04 23:40:48 +0700

    Update Services Route
    Fixing naming method issue

--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
     findOne,
     create,
     update,
+    deleteService,
   } = require("../controllers/services.controller");
   const router = require("express").Router();
 
@@ -11,7 +12,7 @@ module.exports = (app) => {
   router.post("/", create);
   router.get("/:id", findOne);
   router.put("/:id", update);
-  router.delete("/:id", services.delete);
+  router.delete("/:id", deleteService);
 
   app.use("/api/v1/services", router);
 };
