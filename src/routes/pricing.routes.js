885df9764cf0781a5d297f23f824db17499d60ce
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-08-04 23:38:43 +0700

    Update Pricing Route

--- src/routes/pricing.routes.js
+++ src/routes/pricing.routes.js
@@ -1,10 +1,14 @@
 module.exports = (app) => {
-  const { findAll, findOne } = require("../controllers/pricing.controller");
+  const {
+    findAll,
+    findOne,
+    deletePrice,
+  } = require("../controllers/pricing.controller");
   const router = require("express").Router();
 
   router.get("/", findAll);
   router.get("/:id", findOne);
-  router.delete("/:id", pricing.delete);
+  router.delete("/:id", deletePrice);
 
   app.use("/api/v1/pricing", router);
 };
