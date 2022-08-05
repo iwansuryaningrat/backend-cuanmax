22fe195ec98b7eda51952e9c126bcb06ba834f81
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-08-05 21:50:30 +0700

    Update Pricing Router
    Change Code to ES6 Style

--- src/routes/pricing.routes.js
+++ src/routes/pricing.routes.js
@@ -1,14 +1,18 @@
-module.exports = (app) => {
-  const {
-    findAll,
-    findOne,
-    deletePrice,
-  } = require("../controllers/pricing.controller");
-  const router = require("express").Router();
+import {
+  findAll,
+  findOne,
+  deletePrice,
+} from "../controllers/pricing.controller.js";
+import { login, admin, proMember } from "../middlewares/auth.js";
+import Express from "express";
+const router = Express.Router();
 
+const pricingRouter = (app) => {
   router.get("/", findAll);
   router.get("/:id", findOne);
-  router.delete("/:id", deletePrice);
+  router.delete("/:id", login, admin, deletePrice);
 
   app.use("/api/v1/pricing", router);
 };
+
+export default pricingRouter;
