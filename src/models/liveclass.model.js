53eed249867213e5ae5bf124c94b56cad26259c0
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-08-10 11:11:23 +0700

    Edit Liveclass Schema
    Add Liveclass Code & Price field

--- src/models/liveclass.model.js
+++ src/models/liveclass.model.js
@@ -5,6 +5,14 @@ export default (mongoose) => {
         type: String,
         require: true,
       },
+      liveclassCode: {
+        type: String,
+        require: true,
+      },
+      price: {
+        type: Number,
+        require: true,
+      },
       description: {
         type: String,
         require: true,
