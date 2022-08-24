7d5bc1ba13c7399b31b417b5fa5a5506cf759069
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2022-08-24 18:58:47 +0700

    Make Create Transactions Method

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -33,4 +33,40 @@ const createMemberTransaction = (req, res) => {
   }
 };
 
+const createTransaction = (req, res) => {
+  const { memberId } = req.params;
+
+  if (!memberId) {
+    return res.status(400).send({
+      message: "Member id is required",
+    });
+  }
+
+  const { userId, userName, userEmail } = req.body;
+
+  if (!userId || !userName || !userEmail) {
+    return res.status(400).send({
+      message: "User id, user name and user email are required",
+    });
+  }
+
+  const newTransaction = {
+    memberId,
+    userId,
+    userName,
+    userEmail,
+  };
+
+  Transactions.create(newTransaction)
+    .then((transaction) => {
+      res.send(transaction);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the Transaction.",
+      });
+    });
+};
+
 export { transactionsTest, createTransaction };
