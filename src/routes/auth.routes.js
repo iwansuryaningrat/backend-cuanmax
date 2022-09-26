81160b56cf7ba6f9ebbc92b026e72d78fbd07290
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-09-26 11:11:10 +0700

    Define Headers for Auth Response

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,12 @@
 import login from "../controllers/login.controller.js";
 import signup from "../controllers/signup.controller.js";
 import Express from "express";
+import headers from "../services/headers.js";
 
 const authRouter = (app) => {
   const router = Express.Router();
+
+  app.use(headers);
 
   router.post("/login", login);
   router.post("/signup", signup);
