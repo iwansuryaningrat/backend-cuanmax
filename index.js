36b22de71722fc43e089a762c2b9204b50acadf8
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-02 17:18:05 +0000

    Change PORT

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port http://localhost:${PORT}`);
