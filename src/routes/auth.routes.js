59aae23f712489f858cb06c899b002430adec318
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-10 03:33:40 +0700

    Updating Auth Routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,11 @@
-import login from "../controllers/auth/login.controller.js";
+import loggingin from "../controllers/auth/login.controller.js";
 import signup from "../controllers/auth/signup.controller.js";
+import {
+  resetPassword,
+  forgotPassword,
+  resetPasswordWithToken,
+} from "../controllers/auth/resetPassword.controller.js";
+import { login } from "../middlewares/auth.js";
 import Express from "express";
 import headers from "../services/headers.js";
 
@@ -8,8 +14,11 @@ const authRouter = (app) => {
 
   app.use(headers);
 
-  router.post("/login", login);
+  router.post("/login", loggingin);
   router.post("/signup", signup);
+  router.put("/resetPassword", login, resetPassword);
+  router.post("/forgotPassword", forgotPassword);
+  router.put("/resetPassword/:token", resetPasswordWithToken);
 
   app.use("/v1/auth", router);
 };
