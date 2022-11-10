bcd5a048ff63181a0738272d6c8e31d05dcb5a37
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-11 04:16:56 +0700

    Create Basic Users Routes

--- src/routes/users/users.routes.js
+++ src/routes/users/users.routes.js
@@ -1,24 +1,21 @@
 import {
-  findAll,
   findOne,
   update,
   changePassword,
   changeProfilePicture,
-  deleteUSer,
-} from "../controllers/users.controller.js";
-import { login, admin } from "../middlewares/auth.js";
+} from "../../controllers/users.controller.js";
+import { login } from "../../middlewares/auth.js";
+import userFinder from "../../middlewares/usersfinder.js";
 import Express from "express";
 const router = Express.Router();
 
 const usersRouter = (app) => {
-  router.get("/", admin, findAll);
-  router.get("/:id", login, findOne);
-  router.put("/:id", login, update);
-  router.put("/:id/changepassword", login, changePassword);
-  router.put("/:id/changepicture", login, changeProfilePicture);
-  router.delete("/:id", login, admin, deleteUSer);
+  router.get("/:id", login, userFinder, findOne);
+  router.put("/:id", login, userFinder, update);
+  router.put("/:id/changepassword", login, userFinder, changePassword);
+  router.put("/:id/changepicture", login, userFinder, changeProfilePicture);
 
-  app.use("/api/v1/users", router);
+  app.use("/v1/users", router);
 };
 
 export default usersRouter;
