1797d4b8a0a7d335fe31a96f04ecd89af95c16a4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-11 04:10:36 +0700

    Create Find By Id Endpoint for Admin

--- src/routes/admin/users.routes.js
+++ src/routes/admin/users.routes.js
@@ -12,10 +12,10 @@ const router = Express.Router();
 
 const usersAdminRouter = (app) => {
   router.get("/", admin, findAll);
-  router.get("/:id", login, findOne);
-  router.put("/:id", login, update);
-  router.put("/:id/changepassword", login, changePassword);
-  router.put("/:id/changepicture", login, changeProfilePicture);
+  router.get("/:id", login, admin, findOne);
+  router.put("/:id", login, admin, update);
+  router.put("/:id/changepassword", login, admin, changePassword);
+  router.put("/:id/changepicture", login, admin, changeProfilePicture);
   router.delete("/:id", login, admin, deleteUSer);
 
   app.use("/v1/admin/users", router);
