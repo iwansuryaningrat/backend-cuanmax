3f1d25b88935216bbd6517010f6474acc29d5695
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-12 02:04:55 +0700

    Create Verify Account Controller

---
+++ src/controllers/auth/verifyAccount.controller.js
@@ -0,0 +1,37 @@
+import db from "../../models/index.js";
+const Users = db.users;
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+// Verify Account (DONE)
+const verifyAccount = async (req, res) => {
+  const { token } = req.params;
+
+  if (!token) {
+    return res.status(400).send({
+      message: "Token is required",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await Users.findOne({ email: decoded.email });
+    if (!user) {
+      return res.status(404).send({
+        message: "User not found",
+      });
+    }
+    user.type.isActivated = true;
+    await user.save();
+    return res.status(200).send({
+      message: "Account verified successfully",
+      email: user.email,
+    });
+  } catch (error) {
+    return res.status(401).send({
+      message: "Invalid Token",
+    });
+  }
+};
+
+export default verifyAccount;
