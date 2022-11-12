b2f6936730636ffe4716dccf352b564d31b13da8
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-13 03:48:59 +0700

    Add Middleware to Users - Admin Router

--- src/routes/admin/users.routes.js
+++ src/routes/admin/users.routes.js
@@ -7,15 +7,22 @@ import {
   deleteUSer,
 } from "../../controllers/users.controller.js";
 import { login, admin } from "../../middlewares/auth.js";
+import userFinder from "../../middlewares/usersfinder.js";
 import Express from "express";
 const router = Express.Router();
 
 const usersAdminRouter = (app) => {
   router.get("/", admin, findAll);
   router.get("/:id", login, admin, findOne);
-  router.put("/:id", login, admin, update);
-  router.put("/:id/changepassword", login, admin, changePassword);
-  router.put("/:id/changepicture", login, admin, changeProfilePicture);
+  router.put("/:id", login, admin, userFinder, update);
+  router.put("/:id/changepassword", login, admin, userFinder, changePassword);
+  router.put(
+    "/:id/changepicture",
+    login,
+    admin,
+    userFinder,
+    changeProfilePicture
+  );
   router.delete("/:id", login, admin, deleteUSer);
 
   app.use("/v1/admin/users", router);
