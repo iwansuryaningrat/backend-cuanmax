5ad551307ed6d8d990aeb1adaf2dc6bdcbbfc8a3
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-13 22:16:04 +0700

    Update Pro Users Routes

--- src/routes/pro_users/users.routes.js
+++ src/routes/pro_users/users.routes.js
@@ -11,21 +11,21 @@ const router = Express.Router();
 
 const usersProRouter = (app) => {
   router.get("/:id", login, proMember, userFinder, findOne);
-  router.put("/:id", login, proMember, userFinder, update);
-  router.put(
-    "/:id/changepassword",
-    login,
-    proMember,
-    userFinder,
-    changePassword
-  );
-  router.put(
-    "/:id/changepicture",
-    login,
-    proMember,
-    userFinder,
-    changeProfilePicture
-  );
+  // router.put("/:id", login, proMember, userFinder, update);
+  // router.put(
+  //   "/:id/changepassword",
+  //   login,
+  //   proMember,
+  //   userFinder,
+  //   changePassword
+  // );
+  // router.put(
+  //   "/:id/changepicture",
+  //   login,
+  //   proMember,
+  //   userFinder,
+  //   changeProfilePicture
+  // );
 
   app.use("/v1/pro/users", router);
 };
