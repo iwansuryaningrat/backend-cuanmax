e27cd3ea972554bb59424661981f80acffd87d63
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-14 01:29:11 +0700

    Create Change Password Endpoint for Basic Users

--- src/routes/users/users.routes.js
+++ src/routes/users/users.routes.js
@@ -12,7 +12,7 @@ const router = Express.Router();
 const usersRouter = (app) => {
   router.get("/:id", login, userFinder, findOne);
   router.put("/:id", login, userFinder, update);
-  // router.put("/:id/changepassword", login, userFinder, changePassword);
+  router.put("/:id/changepassword", login, userFinder, changePassword);
   // router.put("/:id/changepicture", login, userFinder, changeProfilePicture);
 
   app.use("/v1/users", router);
