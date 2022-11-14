925eefc0e46ab29341a5056d7743ae3a306f17da
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-15 01:33:35 +0700

    Testing Cuy

--- test/unit/time.test.js
+++ test/unit/time.test.js
@@ -1,11 +1,16 @@
 const date1 = new Date("2019-01-01");
-const date2 = new Date();
+const date2 = new Date().getTime();
+const date4 = new Date(date2);
 
 // Generate date for 3 months from now
 const date3 = new Date();
 date3.setMonth(date3.getMonth() + 3);
+const date5 = date3.getTime();
+const date6 = new Date(date5).getTime();
 
-console.log(date1, date1.toString()); // false
-console.log(date2, date2.toString()); // false
-console.log(date3, date3.toString()); // true
-console.log(date1 < date2); // false
+console.log(date1);
+console.log(date2);
+console.log(date3);
+console.log(date4);
+console.log(date5);
+console.log(date6);
