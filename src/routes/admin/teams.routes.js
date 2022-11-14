3b91d3a88a32fbe3cd767a829e5ca99ccb0bcc17
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-15 03:14:38 +0700

    Create Teams Routes for Admin

---
+++ src/routes/admin/teams.routes.js
@@ -0,0 +1,24 @@
+import {
+  findAll,
+  findOne,
+  create,
+  update,
+  teamProfilePicture,
+  deleteTeam,
+} from "../../controllers/teams.controller.js";
+import { login, admin } from "../../middlewares/auth.js";
+import Express from "express";
+const router = Express.Router();
+
+const teamsRouterAdmin = (app) => {
+  router.get("/", findAll);
+  router.post("/", login, admin, create);
+  router.get("/:id", login, admin, findOne);
+  router.put("/:id", login, admin, update);
+  router.put("/:id/teamprofile", login, admin, teamProfilePicture);
+  router.delete("/:id", login, admin, deleteTeam);
+
+  app.use("/v1/admin/teams", router);
+};
+
+export default teamsRouterAdmin;
