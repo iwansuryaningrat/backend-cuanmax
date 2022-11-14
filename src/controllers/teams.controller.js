ae40ec7601b29ee7de69da6f694f369d6e6b4dea
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-15 03:14:15 +0700

    Create Teams Controller

---
+++ src/controllers/teams.controller.js
@@ -0,0 +1,196 @@
+import db from "../models/index.js";
+const Teams = db.teams;
+
+// Fetch all teams data
+const findAll = (req, res) => {
+  Teams.find()
+    .then((result) => {
+      res.send({
+        message: "Teams successfully fetched.",
+        timestamp: new Date().toString(),
+        data: result,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message || "Some error while retrieving teams.",
+      });
+    });
+};
+
+// Fetch one team data
+const findOne = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send({
+      message: "Team ID is required.",
+    });
+  }
+
+  Teams.findById(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).send({
+          message: "Team not found",
+        });
+      }
+
+      res.send({
+        message: "Team successfully retrieved.",
+        timestamp: new Date().toString(),
+        data: result,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message || "Some error while showing teams.",
+      });
+    });
+};
+
+// Delete team
+const deleteTeam = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send({
+      message: "Team ID is required.",
+    });
+  }
+
+  Teams.findByIdAndRemove(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).send({
+          message: "Team not found",
+        });
+      }
+
+      res.send({
+        message: "Team successfully deleted.",
+        timestamp: new Date().toString(),
+      });
+    })
+    .catch((err) => {
+      return res.status(409).send({
+        message: err.message || "Some error while delete team.",
+      });
+    });
+};
+
+// Create and Save a new Team
+const create = (req, res) => {
+  const { name, description, position, email } = req.body;
+
+  if (!name || !description || !position || !email) {
+    return res.status(400).send({
+      message: "Please fill all required fields.",
+    });
+  }
+
+  const team = new Teams({
+    name: name,
+    description: description,
+    position: position,
+    contact: {
+      instagram: req.body.instagram,
+      email: email,
+      twitter: req.body.twitter,
+      linkedin: req.body.linkedin,
+    },
+  });
+
+  team
+    .save(team)
+    .then((result) => {
+      res.status(200).send({
+        message: "Team successfully added.",
+        timestamp: new Date().toString(),
+        data: result,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message || "Some error while creating team.",
+        timestamp: new Date().toString(),
+      });
+    });
+};
+
+// Update a Team by the id in the request
+const update = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send({
+      message: "Teams ID is required",
+    });
+  }
+
+  Teams.findByIdAndUpdate(id, req.body, { new: true })
+    .then((result) => {
+      if (!result) {
+        return res.status(404).send({
+          message: "Team not found",
+        });
+      }
+
+      res.send({
+        message: "Team was updated",
+        timestamp: new Date().toString(),
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message || "Some error while updating team.",
+        timestamp: new Date().toString(),
+      });
+    });
+};
+
+// Upload photo
+const teamProfilePicture = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).send({
+      message: "Teams ID is required",
+    });
+  }
+
+  if (!req.file) {
+    return res.status(422).send({
+      message: "Team profile photo is required.",
+    });
+  }
+
+  const photoName = req.file.filename;
+  const photoLink = `${req.protocol}://${req.get(
+    "host"
+  )}/assets/images/${photoName}`;
+
+  Teams.findByIdAndUpdate(id, { photo: { photoName, photoLink } })
+    .then((result) => {
+      if (!result) {
+        return res.status(404).send({
+          message: "Team not found",
+        });
+      }
+
+      res.send({
+        message: "Team profile photo successfully updated.",
+        timestamp: new Date().toString(),
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message:
+          err.message ||
+          "Some error occurred while changing the profile picture.",
+        timestamp: new Date().toString(),
+      });
+    });
+};
+
+export { findAll, findOne, deleteTeam, create, update, teamProfilePicture };
