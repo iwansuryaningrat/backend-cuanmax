e152e79cf1cb165bd7b2ee9e7add86ee330e8f93
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-16 15:36:19 +0700

    Update Subscribers Routes for Admin

--- src/routes/admin/subscribers.routes.js
+++ src/routes/admin/subscribers.routes.js
@@ -8,18 +8,15 @@ import {
 import { login, admin } from "../middlewares/auth.js";
 import Express from "express";
 const router = Express.Router();
-import headers from "../services/headers.js";
 
 const subscribersRouter = (app) => {
-  app.use(headers);
-
   router.get("/", login, findAll);
   router.get("/:id", login, admin, findOne);
   router.post("/", login, create);
   router.put("/:id", login, admin, update);
   router.delete("/:id", login, admin, deleteSubs);
 
-  app.use("/api/v1/subscribers", router);
+  app.use("/v1/admin/subscribers", router);
 };
 
 export default subscribersRouter;
