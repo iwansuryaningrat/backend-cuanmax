ad01f1a82b65e268f29643393d44ece5d9502456
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-17 15:58:37 +0700

    Update Subscribers Routes for Admin

--- src/routes/admin/subscribers.routes.js
+++ src/routes/admin/subscribers.routes.js
@@ -1,22 +1,20 @@
 import {
   findAll,
   findOne,
-  create,
   update,
   deleteSubs,
-} from "../controllers/subscribers.controller.js";
-import { login, admin } from "../middlewares/auth.js";
+} from "../../controllers/subscribers.controller.js";
+import { login, admin } from "../../middlewares/auth.js";
 import Express from "express";
 const router = Express.Router();
 
-const subscribersRouter = (app) => {
+const subscribersAdminRouter = (app) => {
   router.get("/", login, findAll);
   router.get("/:id", login, admin, findOne);
-  router.post("/", login, create);
   router.put("/:id", login, admin, update);
   router.delete("/:id", login, admin, deleteSubs);
 
   app.use("/v1/admin/subscribers", router);
 };
 
-export default subscribersRouter;
+export default subscribersAdminRouter;
