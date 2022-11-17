c0ed4f30a7a1d4000f6b43b4c64d22260dc38aa5
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-18 01:28:15 +0700

    Finishing Subscribers Routes

--- src/routes/admin/subscribers.routes.js
+++ src/routes/admin/subscribers.routes.js
@@ -1,7 +1,7 @@
 import {
   findAll,
   findOne,
-  update,
+  deactivate,
   deleteSubs,
 } from "../../controllers/subscribers.controller.js";
 import { login, admin } from "../../middlewares/auth.js";
@@ -11,8 +11,8 @@ const router = Express.Router();
 const subscribersAdminRouter = (app) => {
   router.get("/", login, findAll);
   router.get("/:id", login, admin, findOne);
-  router.put("/:id", login, admin, update);
   router.delete("/:id", login, admin, deleteSubs);
+  router.put("/:id", login, admin, deactivate);
 
   app.use("/v1/admin/subscribers", router);
 };
