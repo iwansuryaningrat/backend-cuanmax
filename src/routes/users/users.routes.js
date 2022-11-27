af664e329b83e02e94e68ae3ade0e18fa6f71cea
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-11-27 17:44:56 +0700

    Create user activation req endpoint

--- src/routes/users/users.routes.js
+++ src/routes/users/users.routes.js
@@ -3,6 +3,7 @@ import {
   update,
   changePassword,
   changeProfilePicture,
+  requestUserActivation,
 } from "../../controllers/users.controller.js";
 import { login } from "../../middlewares/auth.js";
 import userFinder from "../../middlewares/usersfinder.js";
@@ -14,6 +15,12 @@ const usersRouter = (app) => {
   router.put("/:id", login, userFinder, update);
   router.put("/:id/changepassword", login, userFinder, changePassword);
   router.put("/:id/changepicture", login, userFinder, changeProfilePicture);
+  router.post(
+    "/:id/requestuseractivation",
+    login,
+    userFinder,
+    requestUserActivation
+  );
 
   app.use("/v1/users", router);
 };
