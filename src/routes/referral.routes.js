6b295c3f03b18c974599b87ca6171a630937143d
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-02 02:06:42 +0700

    Create referral routers for users

---
+++ src/routes/referral.routes.js
@@ -0,0 +1,12 @@
+import { update } from "../controllers/referral.controller.js";
+import { login, proMember } from "../middlewares/auth.js";
+import Express from "express";
+const router = Express.Router();
+
+const referralRouter = (app) => {
+  router.put("/:referralCode", login, proMember, update);
+
+  app.use("/v1/referrals", router);
+};
+
+export default referralRouter;
