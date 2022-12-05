2c30417c5c83d1cb6873a3c14e27e0e56846737c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-06 03:28:58 +0700

    Create Change Referral Code Endpoint

--- src/routes/pro_users/referral.routes.js
+++ src/routes/pro_users/referral.routes.js
@@ -1,10 +1,15 @@
-import { update, requestWD } from "../../controllers/referral.controller.js";
+import {
+  addBankAccount,
+  changeReferralCode,
+  requestWD,
+} from "../../controllers/referral.controller.js";
 import { login, proMember } from "../../middlewares/auth.js";
 import Express from "express";
 const router = Express.Router();
 
 const referralRouter = (app) => {
-  router.put("/:referralCode", login, proMember, update);
+  router.put("/addbank/:referralCode", login, proMember, addBankAccount);
+  router.put("/:referralCode", login, proMember, changeReferralCode);
   router.post("/wd/:referralCode", login, proMember, requestWD);
 
   app.use("/v1/pro/referrals", router);
