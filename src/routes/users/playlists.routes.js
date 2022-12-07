77bb4fa9e1f83aedeb465e0d83c223e5f175084c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-07 21:54:42 +0700

    Finishing Playlist Routes for Basic Users

--- src/routes/users/playlists.routes.js
+++ src/routes/users/playlists.routes.js
@@ -1,11 +1,10 @@
-import { findAll, findOne } from "../../controllers/playlists.controller.js";
+import { findAllforUsers } from "../../controllers/playlists.controller.js";
 import { login } from "../../middlewares/auth.js";
 import Express from "express";
 const router = Express.Router();
 
 const playlistsRouter = (app) => {
-  router.get("/", login, findAll);
-  router.get("/:id", login, findOne);
+  router.get("/", login, findAllforUsers);
 
   app.use("/v1/users/playlists", router);
 };
