6cd89814a0fc1082421536d2a425706e6b4b7c85
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-07 23:45:33 +0700

    Create Videos Routes for Pro Users

---
+++ src/routes/admin/videos.routes.js
@@ -0,0 +1,28 @@
+import {
+  create,
+  findAll,
+  findOne,
+  findByPlaylist,
+  update,
+  deleteVideo,
+  uploadThumbnail,
+  uploadVideo,
+} from "../../controllers/videos.controller.js";
+import { login, admin, proMember } from "../../middlewares/auth.js";
+import Express from "express";
+const router = Express.Router();
+
+const videosAdminRouter = (app) => {
+  router.get("/", login, proMember, findAll);
+  router.get("/:id", login, proMember, findOne);
+  router.get("/:playlistId", login, proMember, findByPlaylist);
+  router.put("/:id", login, admin, update);
+  router.delete("/:id", login, admin, deleteVideo);
+  router.post("/", login, admin, create);
+  router.post("/:id/thumbnail", login, admin, uploadThumbnail);
+  router.post("/:id/video", login, admin, uploadVideo);
+
+  app.use("/v1/admin/videos", router);
+};
+
+export default videosAdminRouter;
