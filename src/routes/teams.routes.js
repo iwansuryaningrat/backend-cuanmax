c81adbf690362d598cc8b95af1955b8576e03b2e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-11 02:06:07 +0700

    Update Routes after merging controller

--- src/routes/teams.routes.js
+++ src/routes/teams.routes.js
@@ -1,9 +1,9 @@
-import findAll from "../controllers/teams.controller.js";
+import { findAllforUsers } from "../controllers/teams.controller.js";
 import Express from "express";
 const router = Express.Router();
 
 const teamsRouter = (app) => {
-  router.get("/", findAll);
+  router.get("/", findAllforUsers);
 
   app.use("/v1/teams", router);
 };
