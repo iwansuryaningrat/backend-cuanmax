c78942ef0ed606038db85e704c8cd624749f470d
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-12 03:25:46 +0700

    Update Endpoint

--- src/routes/pro_users/videos.routes.js
+++ src/routes/pro_users/videos.routes.js
@@ -1,16 +1,16 @@
 import {
-  findAll,
+  findAllPro,
   findOne,
-  findByPlaylist,
+  findByPlaylistPro,
 } from "../../controllers/videos.controller.js";
 import { login, proMember } from "../../middlewares/auth.js";
 import Express from "express";
 const router = Express.Router();
 
 const videosRouter = (app) => {
-  router.get("/", login, proMember, findAll);
+  router.get("/", login, proMember, findAllPro);
   router.get("/:id", login, proMember, findOne);
-  router.get("/:playlistId", login, proMember, findByPlaylist);
+  router.get("/:playlistId", login, proMember, findByPlaylistPro);
 
   app.use("/v1/pro/videos", router);
 };
