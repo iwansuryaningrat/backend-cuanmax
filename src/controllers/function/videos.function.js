2f0fe41656f61260773475a7a21153dd1fea4289
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-12 19:17:26 +0700

    Create some function

--- src/controllers/function/videos.function.js
+++ src/controllers/function/videos.function.js
@@ -14,4 +14,79 @@ const updateVideoUrl = async (videoId, url) => {
     });
 };
 
-export default updateVideoUrl;
+// Update Video Views
+const updateVideoViews = async (videoId) => {
+  const video = await Videos.findById(videoId);
+  if (video) {
+    const views = video.views;
+    return await Videos.findByIdAndUpdate(videoId, {
+      views: views + 1,
+    })
+      .then((result) => {
+        return true;
+      })
+      .catch((err) => {
+        return err.message;
+      });
+  } else {
+    return false;
+  }
+};
+
+// Update Video Likes
+const updateVideoLikes = async (videoId) => {
+  const video = await Videos.findById(videoId);
+  if (video) {
+    const likes = video.likes;
+    return await Videos.findByIdAndUpdate(videoId, {
+      likes: likes + 1,
+    })
+      .then((result) => {
+        return true;
+      })
+      .catch((err) => {
+        return err.message;
+      });
+  } else {
+    return false;
+  }
+};
+
+// Update Video Dislikes
+const updateVideoDislikes = async (videoId) => {
+  const video = await Videos.findById(videoId);
+  if (video) {
+    const dislikes = video.dislikes;
+    return await Videos.findByIdAndUpdate(videoId, {
+      dislikes: dislikes + 1,
+    })
+      .then((result) => {
+        return true;
+      })
+      .catch((err) => {
+        return err.message;
+      });
+  } else {
+    return false;
+  }
+};
+
+const updatePlaylistVideo = async (videoId, playlistId) => {
+  return await Videos.findByIdAndUpdate(videoId, {
+    playlist: playlistId,
+  })
+    .then((result) => {
+      return true;
+    })
+    .catch((err) => {
+      return err.message;
+    });
+};
+
+export {
+  updateVideoUrl,
+  updateVideoViews,
+  updateVideoLikes,
+  updateVideoDislikes,
+  updatePlaylistVideo,
+};
