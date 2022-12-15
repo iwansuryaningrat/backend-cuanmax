8abd0478847de4cc82b391235de8dae18b2e6216
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-16 03:37:54 +0700

    Add Links to Response Pagination

--- src/controllers/subscribers.controller.js
+++ src/controllers/subscribers.controller.js
@@ -20,11 +20,28 @@ const findAll = async (req, res) => {
   const pageLimit = 10;
   const skip = pageLimit * (page - 1);
   const dataCount = await dataCounter(Subscribers, pageLimit, condition);
+
+  var currentPage = page;
+  const nextPage = currentPage++;
+  const prevPage = page - 1;
+
+  const link = `${req.protocol}://${req.get("host")}${req.baseUrl}`;
+  var nextLink = `${link}?page=${nextPage}`;
+  var prevLink = page > 1 ? `${link}?page=${prevPage}` : null;
+  var lastLink = `${link}?page=${dataCount.pageCount}`;
+  var firstLink = `${link}?page=1`;
+
   const pageData = {
     currentPage: page,
     pageCount: dataCount.pageCount,
     dataPerPage: pageLimit,
     dataCount: dataCount.dataCount,
+    links: {
+      next: nextLink,
+      prev: prevLink,
+      last: lastLink,
+      first: firstLink,
+    },
   };
 
   await Subscribers.find(condition)
