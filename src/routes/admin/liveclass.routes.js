105acc1acba0768e1c8462bee881167e42ff82d4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-16 01:32:23 +0700

    Add Update Status Endpoint

--- src/routes/admin/liveclass.routes.js
+++ src/routes/admin/liveclass.routes.js
@@ -5,6 +5,7 @@ import {
   deleteClass,
   update,
   updateThumbnail,
+  changeStatus,
 } from "../../controllers/liveclass.controller.js";
 import { login, admin } from "../../middlewares/auth.js";
 import Express from "express";
@@ -14,9 +15,10 @@ const liveClassAdminRouter = (app) => {
   router.get("/", login, admin, findAll);
   router.get("/:id", login, admin, findOne);
   router.post("/", login, admin, create);
-  router.delete("/:id", login, admin, deleteClass);
   router.put("/:id", login, admin, update);
   router.put("/:id/thumbnail", login, admin, updateThumbnail);
+  router.put("/:id/status", login, admin, changeStatus);
+  router.delete("/:id", login, admin, deleteClass);
 
   app.use("/v1/admin/liveclass", router);
 };
