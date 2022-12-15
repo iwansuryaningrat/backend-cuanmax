b496e303f8aebb28abe4d5fe9b1320820ba7f43c
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-16 01:44:58 +0700

    Change Main Endpoint

--- src/routes/admin/watchlist.routes.js
+++ src/routes/admin/watchlist.routes.js
@@ -5,23 +5,20 @@ import {
   deleteWl,
   nonActivate,
   update,
-} from "../controllers/watchlist.controller.js";
-import { login, admin, proMember } from "../middlewares/auth.js";
+} from "../../controllers/watchlist.controller.js";
+import { login, admin } from "../../middlewares/auth.js";
 import Express from "express";
 const router = Express.Router();
-import headers from "../services/headers.js";
 
-const watchlistRouter = (app) => {
-  app.use(headers);
-
-  router.get("/", login, proMember, findAll);
+const watchlistAdminRouter = (app) => {
+  router.get("/", login, admin, findAll);
   router.post("/", login, admin, create);
-  router.get("/:id", login, proMember, findOne);
+  router.get("/:id", login, admin, findOne);
   router.delete("/:id", login, admin, deleteWl);
   router.put("/:id/nonactivate", login, admin, nonActivate);
   router.put("/:id", login, admin, update);
 
-  app.use("/api/v1/watchlists", router);
+  app.use("/v1/admin/watchlists", router);
 };
 
-export default watchlistRouter;
+export default watchlistAdminRouter;
