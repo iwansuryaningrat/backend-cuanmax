c4f9683f5ac5e91e5be463322032dac4fad27b08
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-16 03:40:14 +0700

    Edit Endpoint

--- src/routes/pro_users/watchlist.routes.js
+++ src/routes/pro_users/watchlist.routes.js
@@ -1,10 +1,10 @@
-import { findAll, findOne } from "../../controllers/watchlist.controller.js";
+import { findAllPro, findOne } from "../../controllers/watchlist.controller.js";
 import { login, proMember } from "../../middlewares/auth.js";
 import Express from "express";
 const router = Express.Router();
 
 const watchlistRouter = (app) => {
-  router.get("/", login, proMember, findAll);
+  router.get("/", login, proMember, findAllPro);
   router.get("/:id", login, proMember, findOne);
 
   app.use("/v1/pro/watchlists", router);
