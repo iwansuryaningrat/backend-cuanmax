e1b10803c3ac596e2ed86437fc58d09bea6afe6f
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-15 18:24:05 +0700

    change endpoint with new function

--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -1,9 +1,12 @@
-import { findAll, findOne } from "../controllers/services.controller.js";
+import {
+  findAllForUsers,
+  findOne,
+} from "../controllers/services.controller.js";
 import Express from "express";
 const router = Express.Router();
 
 const servicesRouter = (app) => {
-  router.get("/", findAll);
+  router.get("/", findAllForUsers);
   router.get("/:id", findOne);
 
   app.use("/v1/services", router);
