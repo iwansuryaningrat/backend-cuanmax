cadf6f59de413b9ce9f372c577de920182a5f91e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-15 18:04:06 +0700

    Update Endpoint

--- src/routes/liveclass.routes.js
+++ src/routes/liveclass.routes.js
@@ -1,9 +1,12 @@
-import { findAll, findOne } from "../controllers/liveclass.controller.js";
+import {
+  findAllForUsers,
+  findOne,
+} from "../controllers/liveclass.controller.js";
 import Express from "express";
 const router = Express.Router();
 
 const liveClassRouter = (app) => {
-  router.get("/", findAll);
+  router.get("/", findAllForUsers);
   router.get("/:id", findOne);
 
   app.use("/v1/liveclass", router);
