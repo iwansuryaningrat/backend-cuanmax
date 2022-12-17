2e84de6c192cf5edaf618631d1de2d41c5d99ae3
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-17 10:21:54 +0700

    Add top Gainers Endpoint

--- src/routes/coinmarketcap.routes.js
+++ src/routes/coinmarketcap.routes.js
@@ -4,6 +4,7 @@ import {
   info,
   price,
   convertCoin,
+  topGainers,
 } from "../controllers/coinmarketcap/coinmarketcap.controller.js";
 import Express from "express";
 const router = Express.Router();
@@ -11,6 +12,7 @@ const router = Express.Router();
 const coinmarketcapRouter = (app) => {
   router.get("/map", map);
   router.get("/latest", latest);
+  router.get("/top-gainers", topGainers);
   router.get("/info", info);
   router.get("/price", price);
   router.get("/convert", convertCoin);
