95b39bd367b581bc91f32c49c906c4991ee2ae95
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-17 09:21:13 +0700

    Create Object test

---
+++ test/unit/object.test.js
@@ -0,0 +1,22 @@
+const data = {
+  quote: {
+    USD: {
+      price: 1172.2616711901164,
+      volume_24h: 9521905961.644857,
+      volume_change_24h: 59.7859,
+      percent_change_1h: -0.07422339,
+      percent_change_24h: -7.7413402,
+      percent_change_7d: -7.51900962,
+      percent_change_30d: -4.09902114,
+      percent_change_60d: -12.06809356,
+      percent_change_90d: -19.35804206,
+      market_cap: 143454192922.47397,
+      market_cap_dominance: 17.8931,
+      fully_diluted_market_cap: 143454192922.47,
+      tvl: null,
+      last_updated: "2022-12-17T01:45:00.000Z",
+    },
+  },
+};
+
+console.log(data.quote.USD);
