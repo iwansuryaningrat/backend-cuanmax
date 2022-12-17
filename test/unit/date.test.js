8f249e6f9edb09d9944a2e30e1ea5d6fbd3efa6a
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-18 04:14:04 +0700

    Create Date Time test

--- test/unit/date.test.js
+++ test/unit/date.test.js
@@ -1,5 +1,26 @@
-const date = "2021-08-01";
-const time = "10:00";
+// Convert 2022-12-17T19:28:38.572Z to 2022-12-17 19:28:38 +0700
+const convert = (date) => {
+  const d = new Date(date);
+  const offset = d.getTimezoneOffset();
+  const offsetHours = Math.floor(offset / 60);
+  const offsetMinutes = offset % 60;
+  const offsetString = `${offsetHours > 0 ? "-" : "+"}${Math.abs(offsetHours)
+    .toString()
+    .padStart(2, "0")}:${offsetMinutes.toString().padStart(2, "0")}`;
+  return `${d.getFullYear()}-${(d.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${d.getDate().toString().padStart(2, "0")} ${d
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${d.getMinutes().toString().padStart(2, "0")}:${d
+    .getSeconds()
+    .toString()
+    .padStart(2, "0")} ${offsetString}`;
+};
 
-console.log(new Date(date).toDateString());
-console.log(new Date(date + " " + time).getTime());
+const date = new Date("2022-12-17T19:28:38.572Z");
+const date2 = new Date("2022-12-17 19:28:38 +0700");
+console.log(date);
+console.log(date2);
+
+console.log(convert(date));
