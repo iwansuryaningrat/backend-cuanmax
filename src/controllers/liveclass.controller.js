5ffff8920a99066308778a2ec57408cea0987024
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-17 19:53:06 +0700

    Fixing Protocol Issue

--- src/controllers/liveclass.controller.js
+++ src/controllers/liveclass.controller.js
@@ -25,7 +25,8 @@ const findAll = async (req, res) => {
   const nextPage = parseInt(page) + 1;
   const prevPage = parseInt(page) - 1;
 
-  const link = `${req.protocol}://${req.get("host")}${req.baseUrl}`;
+  const protocol = req.protocol === "https" ? req.protocol : "https";
+  const link = `${protocol}://${req.get("host")}${req.baseUrl}`;
   var nextLink =
     nextPage > dataCount.pageCount
       ? `${link}?page=${dataCount.pageCount}`
@@ -130,7 +131,8 @@ const findAllForUsers = async (req, res) => {
   const nextPage = parseInt(page) + 1;
   const prevPage = parseInt(page) - 1;
 
-  const link = `${req.protocol}://${req.get("host")}${req.baseUrl}`;
+  const protocol = req.protocol === "https" ? req.protocol : "https";
+  const link = `${protocol}://${req.get("host")}${req.baseUrl}`;
   var nextLink =
     nextPage > dataCount.pageCount
       ? `${link}?page=${dataCount.pageCount}`
@@ -320,8 +322,9 @@ const create = (req, res) => {
     });
   }
 
+  const protocol = req.protocol === "https" ? req.protocol : "https";
   const photoName = req.file.filename;
-  const photoLink = `${req.protocol}://${req.get(
+  const photoLink = `${protocol}://${req.get(
     "host"
   )}/assets/images/${photoName}`;
 
@@ -369,8 +372,9 @@ const create = (req, res) => {
 
 // Update thumbnail (Done)
 const updateThumbnail = (req, res) => {
+  const protocol = req.protocol === "https" ? req.protocol : "https";
   const photoName = req.file.filename;
-  const photoLink = `${req.protocol}://${req.get(
+  const photoLink = `${protocol}://${req.get(
     "host"
   )}/assets/images/${photoName}`;
 
