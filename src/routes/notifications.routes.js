5e0ff36e99ed3277dcbcbc6a58b06aff6c37b9b6
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-18 01:37:59 +0700

    Fixing Error

--- src/routes/notifications.routes.js
+++ src/routes/notifications.routes.js
@@ -2,9 +2,9 @@ import notificationController from "../controllers/notification.controller.js";
 import Express from "express";
 const router = Express.Router();
 
-const notificationRouter = (app) => {
+const notificationsRouter = (app) => {
   app.post("/", notificationController);
-  app.use("/v1/notification", router);
+  app.use("/v1/notifications", router);
 };
 
-export default notificationRouter;
+export default notificationsRouter;
