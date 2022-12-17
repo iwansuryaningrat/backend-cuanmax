cf298fbaf60c9d1cab2a7ab67e90f92e4cc28e12
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-17 23:30:04 +0700

    Create Vouchers Route for Users

---
+++ src/routes/vouchers.routes.js
@@ -0,0 +1,22 @@
+import {
+  findAll,
+  create,
+  deleteVoucher,
+  update,
+  findOne,
+} from "../controllers/vouchers.controller.js";
+import { login, admin } from "../middlewares/auth.js";
+import Express from "express";
+const router = Express.Router();
+
+const vouchersRouter = (app) => {
+  router.get("/", login, admin, findAll);
+  router.post("/", login, admin, create);
+  router.get("/:id", login, findOne);
+  router.delete("/:id", login, admin, deleteVoucher);
+  router.put("/:id", login, admin, update);
+
+  app.use("/v1/vouchers", router);
+};
+
+export default vouchersRouter;
