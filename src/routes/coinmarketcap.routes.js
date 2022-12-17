3622a8e5cd9dc14f970b122b72fe80746b81e7e4
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-17 08:14:35 +0700

    Create Coinmarketcap Router

---
+++ src/routes/coinmarketcap.routes.js
@@ -0,0 +1,21 @@
+import {
+  map,
+  latest,
+  info,
+  price,
+  convertCoin,
+} from "../controllers/coinmarketcap/coinmarketcap.controller.js";
+import Express from "express";
+const router = Express.Router();
+
+const coinmarketcapRouter = (app) => {
+  router.get("/map", map);
+  router.get("/latest", latest);
+  router.get("/info", info);
+  router.get("/price", price);
+  router.get("/convert", convertCoin);
+
+  app.use("/v1/cryptocurrency", router);
+};
+
+export default coinmarketcapRouter;
