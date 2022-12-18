c83d8ecb39eb23ab95a1ff416900a6e43218bcb6
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2022-12-19 02:51:16 +0700

    Create Transaction Routes

---
+++ src/routes/admin/transactions.routes.js
@@ -0,0 +1,13 @@
+import { create } from "../../controllers/liveclassTransaction.controller.js";
+import { login, admin } from "../../middlewares/auth.js";
+
+import Express from "express";
+const router = Express.Router();
+
+const transactionAdminRouter = (app) => {
+  router.post("/:userId", create);
+
+  app.use("/v1/admin/transaction", router);
+};
+
+export default transactionAdminRouter;
---
+++ src/routes/transactions.routes.js
@@ -0,0 +1,12 @@
+import { create } from "../controllers/liveclassTransaction.controller.js";
+
+import Express from "express";
+const router = Express.Router();
+
+const transactionRouter = (app) => {
+  router.post("/:userId", create);
+
+  app.use("/v1/transaction", router);
+};
+
+export default transactionRouter;
