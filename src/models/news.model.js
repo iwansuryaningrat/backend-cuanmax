d8cbee0f34cf463a3a33da01aae387cb650c5a6b
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-01-02 10:56:46 +0700

    Update News Model

--- src/models/news.model.js
+++ src/models/news.model.js
@@ -14,9 +14,7 @@ export default (mongoose) => {
         type: String,
         require: true,
       },
-      tags: {
-        type: [String],
-      },
+      tags: [{ type: String }],
       date: {
         type: Date,
         require: true,
